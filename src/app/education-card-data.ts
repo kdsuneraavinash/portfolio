5d229faf878e09849a6d30636154d9c158feafc0
K.D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2019-07-07 19:30:01 +0530

    Refined education page

--- src/app/education-card-data.ts
+++ src/app/education-card-data.ts
@@ -1,4 +1,4 @@
 
 export class EducationCardData {
-    constructor(public title: string, public location: string, public period: string, public content: string) { }
+    constructor(public title: string, public location: string, public period: string, public content: string, public image: string) { }
 }
--- src/app/education-data.ts
+++ src/app/education-data.ts
@@ -3,6 +3,7 @@ export let educationData = [
         title: "UNDERGRADUATE, BACHELOR OF SCIENCE",
         location: "University of Moratuwa",
         period: "2017 - Current",
+        image: "https://i0.wp.com/studentlanka.com/wp-content/uploads/2014/03/University-of-Moratuwa-Sumanadasa-Building.jpg",
         content:
             `Undergraduate, Bachelor of Science, Computer Science and Engineering, University of Moratuwa.
             <br/> 
@@ -13,6 +14,7 @@ export let educationData = [
         title: "GCE ADVANCED LEVEL EXAMINATION",
         location: "Kalutara Vidyalaya - National School",
         period: "2003 - 2016",
+        image: "http://www.sundaytimes.lk/130714/uploads/37-300x225.jpg",
         content:
             `
             All A Passes in Physical Sciences Stream<br/> 
